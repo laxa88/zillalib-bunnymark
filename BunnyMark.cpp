6d3f28bffb69079785338e3db5b1ceda182f8cd1
LWY <668915+laxa88@users.noreply.github.com>
2020-07-05 02:09:28 +0900

    add initial complete BunnyMark implementation

--- BunnyMark.cpp
+++ BunnyMark.cpp
@@ -14,34 +14,72 @@ void BunnyMark::Init() {
 	rectfBounds.high = 0.0;
 	rectfBounds.right = ZLWIDTH;
 	rectfBounds.low = ZLHEIGHT;
+	bAdding = false;
+	nCount = 0;
+	nMaxCount = 200000;
+	nAmount = 100;
 
 	srfBunnyTiles = ZL_Surface("Data/lineup.png");
 	srfBunnyTiles.SetTextureFilterMode(false, false);
-	srfBunnyTiles.SetTilesetClipping(35, 36);
+	srfBunnyTiles.SetTilesetClipping(12, 0);
 	nCount = 0;
 
 	// if (startBunnyCount > 0)
 	{
-		AddBunnies(10000);
+		AddBunnies(100);
 	}
 }
 
 void BunnyMark::Update()
 {
-	nCount += 1;
+    if (bAdding)
+    {
+        if (nCount < nMaxCount)
+        {
+            AddBunnies(nAmount);
+        }
+    }
+
+    for (auto &bunny : vecBunnies)
+    {
+        bunny.Update();
+    }
 }
 
 void BunnyMark::Draw()
-{}
+{
+	for (auto &bunny : vecBunnies)
+    {
+        srfBunnyTiles.SetTilesetIndex(bunny.nSpriteIndex);
+		srfBunnyTiles.Draw(bunny.dx, bunny.dy);
+    }
+}
 
 void BunnyMark::AddBunnies(int count)
-{}
+{
+	for (int i = 0; i < count; i++)
+    {
+		Bunny bunny{ZL_Rand::Int(12), rectfBounds};
+		bunny.dx = (nCount % 2) * 800;
+		vecBunnies.push_back(bunny);
+		nCount++;
+    }
+}
 
 void BunnyMark::StartAdding()
-{}
+{
+	bAdding = true;
+}
 
 void BunnyMark::StopAdding()
-{}
+{
+	bAdding = false;
+}
 
 void BunnyMark::Reset()
-{}
+{
+	// TODO delete each bunny before clearing?
+
+	vecBunnies.clear();
+	nCount = 0;
+}
