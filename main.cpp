33786b132a4b6fd2869e6632353c42631840e039
LWY <668915+laxa88@users.noreply.github.com>
2020-07-05 13:40:39 +0900

    refactor main

--- main.cpp
+++ main.cpp
@@ -5,9 +5,11 @@ ZL_Font fnt;
 
 struct sZillaBunnyMark : public ZL_Application
 {
-	void Load(int argc, char *argv[])
+	sZillaBunnyMark() : ZL_Application(60) {}
+
+	void Load(int argc, char* argv[])
 	{
-		ZL_Display::Init("Zilla Bunny Mark", 800, 800);
+		ZL_Display::Init("Zilla Bunny Mark", 780, 600);
 		ZL_Display::sigPointerDown.connect(this, &sZillaBunnyMark::onPointerDown);
 		ZL_Display::sigPointerUp.connect(this, &sZillaBunnyMark::onPointerUp);
 		ZL_Display::SetAA(false);
@@ -16,6 +18,7 @@ struct sZillaBunnyMark : public ZL_Application
 		BunnyMark::Init(100);
 
 		fnt = ZL_Font("Data/gbfont.png");
+		fnt.SetColor(ZL_Color::DarkGreen);
 	}
 
 	void BeforeFrame()
@@ -25,20 +28,18 @@ struct sZillaBunnyMark : public ZL_Application
 
 	void AfterFrame()
 	{
-		ZL_Display::ClearFill(ZL_Color::Gray);
-		fnt.Draw(8, ZLHEIGHT-8, ZL_String::format("Elapsed: %f\nBunnies: %d", ZLSECONDS, BunnyMark::nCount), ZL_Origin::TopLeft);
+		ZL_Display::ClearFill(ZL_Color::White);
+		fnt.Draw(8, ZLHEIGHT-8, ZL_String::format("Bunnies: %d", BunnyMark::nCount), ZL_Origin::TopLeft);
 		BunnyMark::Draw();
 	}
 
 	void onPointerDown(ZL_PointerPressEvent& e)
 	{
-		ZL_LOG("DEBUG", "pointer down");
 		BunnyMark::StartAdding();
 	}
 
 	void onPointerUp(ZL_PointerPressEvent& e)
 	{
-		ZL_LOG("DEBUG", "pointer up");
 		BunnyMark::StopAdding();
 	}
 } ZillaBunnyMark;
